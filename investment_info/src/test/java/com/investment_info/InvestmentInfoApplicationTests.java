aea6fe845385b4f48a4ff314d3e77778bc83f5c6
abcals <abcals/my_test.git>
2021-08-20 12:48:45 +0900

    투자 정보 프로젝트 생성

---
+++ investment_info/src/test/java/com/investment_info/InvestmentInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investment_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
